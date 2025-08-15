519b58232b1d11a3fba5ef4c8c471ec0902a9c1b
Lauren-Mercedes <lauren.m.smith1310@gmail.com>
2025-08-15 10:16:24 +0100

    Add carousel markers, hide scroll bar, implement js function to change colour and adjust for larger screen sizes.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,9 +9,42 @@ function left() {
     const carousel = document.getElementById('carousel');
     const scrollAmount = carousel.offsetWidth;
     carousel.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+    // call function to update marker colour
+    updateMarkers();
 }
 function right() {
     const carousel = document.getElementById('carousel');
     const scrollAmount = carousel.offsetWidth;
     carousel.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+    // call function to update marker colour
+    updateMarkers();
 }
+
+
+/**
+ * Update markers based on the current active item in the carousel.
+ * Current active item is calculated by dividing the scrollLeft position of the carousel by the width of the carousel.
+ * Markers are updated to reflect the active item by changing their background color (primary color for active, secondary color for inactive).
+ * This function is called on scroll and when buttons are clicked.
+ */
+function updateMarkers() {
+    const carousel = document.getElementById('carousel');
+    const markers = document.querySelectorAll('.marker');
+    const scrollLeft = carousel.scrollLeft;
+    const itemWidth = carousel.offsetWidth;
+    const activeIndex = Math.round(scrollLeft / itemWidth);
+
+    markers.forEach((marker, index) => {
+        if (index === activeIndex) {
+            marker.style.backgroundColor = 'var(--primary-color)';
+        }
+        else {
+            marker.style.backgroundColor = 'var(--secondary-color)';
+        };
+        // Update markers on scroll
+        carousel.addEventListener('scroll', updateMarkers);
+    });
+}
+
+// initial to set the correct starting marker state.
+updateMarkers()
